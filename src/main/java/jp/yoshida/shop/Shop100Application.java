b26fcd50824d89c0ae32e15baa10b61273e6bb27
Natsumi Matsumoto <231039@st.yoshida-g.ac.jp>
2023-07-25 11:15:17 +0900

    first commit

---
+++ src/main/java/jp/yoshida/shop/Shop100Application.java
@@ -0,0 +1,13 @@
+package jp.yoshida.shop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Shop100Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Shop100Application.class, args);
+	}
+
+}
